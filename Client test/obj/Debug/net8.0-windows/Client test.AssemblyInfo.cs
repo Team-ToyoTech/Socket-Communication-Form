0fd79d452466c4defc6fb83b0269b8d68c784b16
endiy0 <steve060401@gmail.com>
2025-04-10 20:10:48 +0900

    v3.4
    re commit

--- Client test/obj/Debug/net8.0-windows/Client test.AssemblyInfo.cs
+++ Client test/obj/Debug/net8.0-windows/Client test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8315e807617b5ede93744a12e0d29e084adec236")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
